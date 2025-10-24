8a281400d16ac90abd2b6931497c1aedeba24ff7
Heinriches <heydrich.institut@gmail.com>
2025-10-24 19:39:47 -0300

    Create Main.c

---
+++ (sololearn) Introduction to C/module_02/lesson_02/example_03/Main.c
@@ -0,0 +1,28 @@
+/**
+ * ELSE STATEMENT
+ * You can use the 'else' statement after an 'if' statement
+ * if you want to run a code in case the condition is not satisfied.
+ * 
+ * For example:
+ */
+
+
+#include <stdio.h>
+int main()
+{
+  int score = 87;
+  
+  if(score >= 100){
+    printf("Level Completed!");
+  }
+  else{
+    printf("Game Over");
+  }
+  
+  return 0;
+}
+
+/**
+ * Open and run the code to see how it works, based on the value
+ * of the variable.
+ */
