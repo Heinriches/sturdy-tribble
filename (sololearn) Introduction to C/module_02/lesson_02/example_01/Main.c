d3b4c605b890ef53eeba12264be415ad4cdd5340
Heinriches <heydrich.institut@gmail.com>
2025-10-24 19:18:43 -0300

    Create Main.c

---
+++ (sololearn) Introduction to C/module_02/lesson_02/example_01/Main.c
@@ -0,0 +1,34 @@
+/**
+ * IF STATEMENT
+ * The following comparison operators may be used 
+ * to form the condition:
+ * '&lt' less than
+ * '&gt' greater than
+ * '!=' not equal to
+ * '==' equal to
+ * '&lt;=' less than or equal to
+ * '&gt;=' greater than or equal to
+ * 
+ * For example:
+ */
+
+
+#include <stdio.h>
+int main()
+{
+  int score = 142;
+  if(score >= 100){
+    printf("Level Completed!");
+  }
+  
+  return 0;
+}
+
+
+/**
+ * The message will be output only if the condition 
+ * is satisfied.
+ * 
+ * Open the code and try changing the value of the 
+ * 'score' variable, to see how it works.
+ */
