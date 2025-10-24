180334fc8f53de2c066d2cc20a6ac31fa6b6f882
Heinriches <heydrich.institut@gmail.com>
2025-10-24 19:24:49 -0300

    Create Main.c

---
+++ (sololearn) Introduction to C/module_02/lesson_02/example_02/Main.c
@@ -0,0 +1,21 @@
+/**
+ * IF STATEMENT
+ * Remember that you need to use two equal equal signs '=='
+ * to test for equality, since a single equal sign is 
+ * the assignment operator.
+ * 
+ * For example, let's check the position value for equality:
+ */
+
+
+#include <stdio.h>
+int main()
+{
+  int position = 2;
+  
+  if(position ==2){
+    printf("Silver");
+  }
+  
+  return 0;
+}
